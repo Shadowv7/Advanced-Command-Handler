2401f4e2181ec62acfe655d1e7fe681fb4ed49e5
Shadowv7 <60565794+Shadowv7@users.noreply.github.com>
2020-03-10 18:11:48 +0400

    Update getThing.js

--- utils/getThing.js
+++ utils/getThing.js
@@ -16,50 +16,50 @@ module.exports = async (dataType, text) => {
 					|| c.aliases && c.aliases.includes(text))
 				|| false;
 		case 'channel':
-			return message.guild.channels.get(text)
+			return message.guild.channels.cache.get(text)
 				|| message.mentions.channels.first()
-				|| message.guild.channels.find((c) => {
+				|| message.guild.channels.cache.find((c) => {
 					return c.name.toLowerCase().includes(text.toLowerCase()) && text.length > 1;
 				})
 				|| false;
 		case 'guild':
-			return client.guilds.get(text)
-				|| client.guilds.find((g) => g.name.toLowerCase().includes(text.toLowerCase()) && text.length > 1)
+			return client.guilds.cache..get(text)
+				|| client.guilds.cache.find((g) => g.name.toLowerCase().includes(text.toLowerCase()) && text.length > 1)
 				|| false;
 		case 'member':
-			return message.guild.members.get(text)
+			return message.guild.members.cache.get(text)
 				|| message.mentions.members.first()
-				|| message.guild.members.find((m) => (m.displayName.toLowerCase().includes(text.toLowerCase())
+				|| message.guild.members.cache.find((m) => (m.displayName.toLowerCase().includes(text.toLowerCase())
 					|| m.user.username.toLowerCase().includes(text.toLowerCase()))
 					&& text.length > 1)
 				|| false;
 		case 'user':
-			return client.users.get(text)
-				|| client.users.find((u) => u.username.toLowerCase() === text.toLowerCase())
+			return client.users.cache.get(text)
+				|| client.users.cache.find((u) => u.username.toLowerCase() === text.toLowerCase())
 				|| message.mentions.users.first()
 				|| false;
 		case 'role':
-			return message.guild.roles.get(text)
+			return message.guild.roles.cache.get(text)
 				|| message.mentions.roles.first()
-				|| message.guild.roles.find((r) => r.name.toLowerCase().includes(text.toLowerCase()) && text.length > 1)
+				|| message.guild.roles.cache.find((r) => r.name.toLowerCase().includes(text.toLowerCase()) && text.length > 1)
 				|| false;
 		case 'emote':
-			return client.emojis.get(text)
-				|| client.emojis.find((e) => e.name.toLowerCase().includes(text.toLowerCase()) && text.length > 1)
+			return client.emojis.cache.get(text)
+				|| client.emojis.cache.find((e) => e.name.toLowerCase().includes(text.toLowerCase()) && text.length > 1)
 				|| false;
 		case 'message':
 			
 			if (text.length < 7) return false;
-			const m = await message.channel.fetchMessage(text);
+			const m = await message.channel.messages.fetch(text);
 			if (m) return m;
 			
 			const url = message.replace('https://discordapp.com/channels/', '').split('/');
 			if (message.startsWith('https') && client.channels.has(url[1])) {
-				return (await client.channels.get(url[1]).fetchMessage(url[2])) || false;
+				return (await client.channels.get(url[1]).messages.fetch(url[2])) || false;
 			}
 			
 			for (const channel of client.channels) {
-				const m = await channel[1].fetchMessage(text);
+				const m = await channel[1].message.fetch(text);
 				if (m) return m;
 			}
 			return false;
